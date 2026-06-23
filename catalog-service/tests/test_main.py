094bece00acf897718275887b014458d564a3748
zdzhatdo <157390414+zdzhatdo@users.noreply.github.com>
2026-06-23 19:22:56 -0400

    Add first passing test for /health

--- catalog-service/tests/test_main.py
+++ catalog-service/tests/test_main.py
@@ -5,3 +5,8 @@ from fastapi.testclient import TestClient
 from app.main import app
 
 client = TestClient(app)
+
+def test_health_check():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
