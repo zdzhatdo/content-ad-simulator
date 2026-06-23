3917f14c8bcf74eacaf415c968890e9385116813
zdzhatdo <157390414+zdzhatdo@users.noreply.github.com>
2026-06-23 16:49:54 -0400

    Move database URL to environment variable

--- catalog-service/app/database.py
+++ catalog-service/app/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .models import Base, Content, AdSlot
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:devpassword@localhost:5433/catalog"
+load_dotenv()
+
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_engine(DATABASE_URL)
 
